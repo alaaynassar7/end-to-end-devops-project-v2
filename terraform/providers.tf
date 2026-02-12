a19a410ba1826e312e61b799c001a9ac7d5196ef
AzurePipeline <pipeline-bot@alaa.com>
2026-02-12 02:39:30 +0200

    Cleaned terraform state and re-initialized

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,32 +1,30 @@
 terraform {
-  required_version = ">= 1.5.0" 
-
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "> 5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
+      version = "> 2.23"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.11"
+      version = "> 2.11"
     }
   }
-}
-
+ }
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-2"   
 }
 
 data "aws_eks_cluster_auth" "main" {
-  name = module.compute.cluster_name
+  name = aws_eks_cluster.main.name
 }
 
 provider "kubernetes" {
-  host                   = module.compute.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.compute.cluster_certificate_authority_data)
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
 }
